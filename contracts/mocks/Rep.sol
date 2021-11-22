bceffe99f41742cd31eae33d236a02e769c30c5b
nico-limo <nicolas.limonoff@gmail.com>
2021-11-22 15:17:37 -0300

    tests

--- contracts/mocks/Rep.sol
+++ contracts/mocks/Rep.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract REP is ERC20 {
+contract Rep is ERC20 {
     constructor() ERC20('REP','Augur token') {}
     
     function faucet(address to, uint amount) external {
